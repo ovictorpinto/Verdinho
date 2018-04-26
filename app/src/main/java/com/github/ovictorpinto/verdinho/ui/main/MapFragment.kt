89450188b4b076b49f8c77327584bcf7a6f3ec1a
Victor Pinto <ovictorpinto@gmail.com>
2018-04-25 23:20:23 -0300

    Inicio das opções de pesquisar destino a partir de um ponto de origem

--- app/src/main/java/com/github/ovictorpinto/verdinho/ui/main/MapFragment.kt
+++ app/src/main/java/com/github/ovictorpinto/verdinho/ui/main/MapFragment.kt
@@ -2,11 +2,13 @@ package com.github.ovictorpinto.verdinho.ui.main
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Fragment
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v13.app.FragmentCompat
 import android.support.v4.app.ActivityCompat
@@ -17,10 +19,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.Toast
+import br.com.tcsistemas.common.net.HttpHelper
+import com.github.ovictorpinto.ConstantesEmpresa
 import com.github.ovictorpinto.verdinho.Constantes
 import com.github.ovictorpinto.verdinho.R
 import com.github.ovictorpinto.verdinho.persistencia.dao.PontoDAO
 import com.github.ovictorpinto.verdinho.persistencia.dao.PontoFavoritoDAO
+import com.github.ovictorpinto.verdinho.retorno.RetornoPesquisarPontos
 import com.github.ovictorpinto.verdinho.to.PontoTO
 import com.github.ovictorpinto.verdinho.ui.ponto.DetalhePontoDialogFrag
 import com.github.ovictorpinto.verdinho.util.LogHelper
@@ -36,6 +41,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.clustering.view.DefaultClusterRenderer
+import kotlinx.android.synthetic.main.ly_map.view.*
 import kotlinx.android.synthetic.main.ly_teste.view.*
 import java.util.*
 
@@ -43,7 +49,45 @@ import java.util.*
  * Created by victorpinto on 09/04/18.
  */
 
-class MapFragment : MapFragment(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+class MapFragment : MapFragment(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        DetalhePontoDialogFrag.PontoDialogoListener {
+
+    var retornoPesquisarPontos: RetornoPesquisarPontos? = null
+
+    override fun onDestino(pontoTO: PontoTO) {
+        Toast.makeText(activity, "Procurando destinos do ponto ${pontoTO.getNomeApresentacao(activity)}", Toast.LENGTH_SHORT).show()
+        ProcessoDestino(activity, pontoTO).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+    }
+
+    inner class ProcessoDestino(var context: Context, var pontoTO: PontoTO) : AsyncTask<Void, String, Boolean>() {
+
+        override fun doInBackground(vararg p0: Void?): Boolean {
+            val mapper = MainActivity.mapper
+
+            val url = ConstantesEmpresa.listarPontos
+            val urlParam = "{\"pontoDeOrigemId\": ${pontoTO.idPonto}}"
+            val headers = ConstantesEmpresa(context).headers
+            LogHelper.log(TAG, url)
+            LogHelper.log(TAG, urlParam)
+
+            val retorno = HttpHelper.doPost(url, urlParam, HttpHelper.UTF8, headers)
+            LogHelper.log(TAG, retorno)
+
+            retornoPesquisarPontos = mapper.readValue(retorno, RetornoPesquisarPontos::class.java)
+            LogHelper.log(TAG, retornoPesquisarPontos!!.getPontosDeParada().size.toString() + " item(s)")
+
+            return true
+        }
+
+        override fun onPostExecute(result: Boolean?) {
+            super.onPostExecute(result)
+            if (!isCancelled) {
+
+                fillMarkers()
+                layout.button_cancelar.visibility = View.VISIBLE
+            }
+        }
+    }
 
     companion object {
         val ZOOM = 16f
@@ -74,8 +118,10 @@ class MapFragment : MapFragment(), OnMapReadyCallback, GoogleApiClient.Connectio
         LocalBroadcastManager.getInstance(activity).registerReceiver(favoriteReceive, IntentFilter(Constantes.actionUpdatePontoFavorito))
     }
 
+    private lateinit var layout: LinearLayout
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val layout = inflater!!.inflate(R.layout.ly_map, container, false) as LinearLayout
+        layout = inflater!!.inflate(R.layout.ly_map, container, false) as LinearLayout
         layout.toolbar.setTitle(R.string.app_name)
 
         val v = super.onCreateView(inflater, container, savedInstanceState)
@@ -87,7 +133,7 @@ class MapFragment : MapFragment(), OnMapReadyCallback, GoogleApiClient.Connectio
         map = mMap
         mClusterManager = ClusterManager(activity, mMap)
         mClusterManager!!.renderer = PontoRenderer(activity, mMap, mClusterManager!!)
-        mClusterManager!!.setOnClusterItemClickListener(PontoPinClickListener())
+        mClusterManager!!.setOnClusterItemClickListener(PontoPinClickListener(this))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(POSICAO_SEDE, ZOOM))
         mMap.setOnCameraIdleListener(mClusterManager)
         mMap.setOnMarkerClickListener(mClusterManager)
@@ -120,7 +166,9 @@ class MapFragment : MapFragment(), OnMapReadyCallback, GoogleApiClient.Connectio
                 for (i in list.indices) {
                     val pontoPO = list[i]
                     val to = pontoPO.pontoTO
-                    it.addItem(to)
+                    //se não houver filtro ou o ponto estiver na lista de filtrados
+                    if (retornoPesquisarPontos == null || retornoPesquisarPontos?.pontosDeParada?.contains(to.idPonto)!!)
+                        it.addItem(to)
                 }
                 it.cluster()
             }
@@ -177,7 +225,7 @@ class MapFragment : MapFragment(), OnMapReadyCallback, GoogleApiClient.Connectio
         val mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient)
         if (mLastLocation != null) {
             LogHelper.log(TAG, "Encontrou última posição!");
-            var devicePosition = LatLng(mLastLocation.latitude, mLastLocation.longitude)
+            val devicePosition = LatLng(mLastLocation.latitude, mLastLocation.longitude)
             map?.moveCamera(CameraUpdateFactory.newLatLng(devicePosition))
             val update = CameraUpdateFactory.zoomTo(ZOOM)
             moveuLocal = true
@@ -192,7 +240,7 @@ class MapFragment : MapFragment(), OnMapReadyCallback, GoogleApiClient.Connectio
             })
 
         } else {
-            Toast.makeText(activity, R.string.location_not_found, Toast.LENGTH_SHORT).show();
+            Toast.makeText(activity, R.string.location_not_found, Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -236,13 +284,15 @@ class MapFragment : MapFragment(), OnMapReadyCallback, GoogleApiClient.Connectio
     /**
      * Define a regra ao clicar no marker do mapa
      */
-    private inner class PontoPinClickListener : ClusterManager.OnClusterItemClickListener<PontoTO> {
+    private inner class PontoPinClickListener(var listener: Fragment) : ClusterManager
+    .OnClusterItemClickListener<PontoTO> {
 
         override fun onClusterItemClick(clicado: PontoTO): Boolean {
             val bundle = Bundle()
             bundle.putSerializable(PontoTO.PARAM, clicado)
 
             val frag = DetalhePontoDialogFrag()
+            frag.setTargetFragment(listener, 1)
             frag.arguments = bundle
             frag.show(fragmentManager, DetalhePontoDialogFrag.TAG_FRAG)
             return true
